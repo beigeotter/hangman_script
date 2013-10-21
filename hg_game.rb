a67156d560404db9a44e29f0ad640a036ecd5153
Josh Frye <jfrye@digitalocean.com>
2013-10-21 17:31:30 -0400

    Cleanup

--- hg_game.rb
+++ hg_game.rb
@@ -1,49 +1,23 @@
-words = ["ant", "baboon", "badger", "bat", "bear", "beaver", "camel", "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer", "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat", "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose", "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", "python", "rabbit", "ram", "rat", "raven", "rhino", "salmon", "seal", "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan", "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf", "wombat", "zebra"]
-puts "\e[H\e[2J"
-puts "WELCOME TO HANGMAN"
-word_to_guess = words.sample
-dash = "_ "
-blanks = dash * word_to_guess.length
-array_to_guess = word_to_guess.chars.to_a
-puts blanks
-
-
-guessed_wrong = Array.new
-guessed_right = Array.new
-array_to_guess = word_to_guess.chars.to_a #an array from the word itself
-#results = Array.new
-
-
 def already_guessed(guessed_letter, guessed_right, guessed_wrong)
-  
-  if guessed_right.include?(guessed_letter)
-      puts "You already guessed this letter. Guess another"
+  if guessed_right.include?(guessed_letter) || guessed_wrong.include?(guessed_letter)
+    puts "You already guessed this letter. Guess another"
   end
-    
-  if guessed_wrong.include?(guessed_letter)
-      puts "You already wrongly guessed this letter. Guess another"
-  end
-  
 end
 
 def is_it_in_the_word(word_to_guess, guessed_letter, guessed_right, guessed_wrong)
-
   if word_to_guess.include?(guessed_letter)
-      guessed_right.push(guessed_letter)
-      puts "Wrong #{guessed_wrong}"
-      puts "Right #{guessed_right}"
+    guessed_right.push(guessed_letter)
   else
-      guessed_wrong.push(guessed_letter)
-      puts "Wrong #{guessed_wrong}"
-      puts "Right #{guessed_right}"
-      
+    guessed_wrong.push(guessed_letter) if !guessed_wrong.include?(guessed_letter)
   end
 
+  puts "Wrong #{guessed_wrong}"
+  puts "Right #{guessed_right}"
 end
 
 def printing_image(guessed_wrong)
   which_hang = guessed_wrong.length
-  
+
   hang_1 = "  +---+
     |   |
         |
@@ -94,59 +68,59 @@ def printing_image(guessed_wrong)
    / \\  |
         |
   ========="
-  
-  
-  if which_hang == 1
+
+  case which_hang
+  when 1
     puts hang_1
-  end
-  
-  if which_hang == 2
+  when 2
     puts hang_2
-  end
-  
-  if which_hang == 3
+  when 3
     puts hang_3
-  end
-  
-  if which_hang == 4
+  when 4
     puts hang_4
-  end
-  
-  if which_hang == 5
+  when 5
     puts hang_5
-  end
-  
-  if which_hang == 6
+  when 6
     puts hang_6
-  end
-  
-  if which_hang == 7
+  when 7
     puts hang_7
   end
-
 end
-  
 
 def print_guess_results(array_to_guess, guessed_right)
   results = Array.new
   array_to_guess.each do |l|
     if guessed_right.include?(l)
       results.push(l)
-    else 
+    else
       results.push("_ ")
     end
   end
- results = results.join(",")
- return "#{results}"
+  results = results.join(",")
+
+  results
 end
 
 def is_the_game_over(guessed_wrong)
   if guessed_wrong.length < 7
     return "game_continues"
-  else 
+  else
     return "game over"
   end
 end
+
+words = ["ant", "baboon", "badger", "bat", "bear", "beaver", "camel", "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer", "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat", "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose", "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", "python", "rabbit", "ram", "rat", "raven", "rhino", "salmon", "seal", "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan", "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf", "wombat", "zebra"]
+puts "\e[H\e[2J"
+puts "WELCOME TO HANGMAN"
+word_to_guess = words.sample
+dash = "_ "
+blanks = dash * word_to_guess.length
+array_to_guess = word_to_guess.chars.to_a
+puts blanks
+
+guessed_wrong = Array.new
+guessed_right = Array.new
+array_to_guess = word_to_guess.chars.to_a #an array from the word itself
 
 while guessed_wrong.length < 7
   puts "Guess a letter"
@@ -172,11 +146,9 @@ while guessed_wrong.length < 7
     puts "YOU WON!!"
     break
   end
-
 end
 
 if guessed_wrong.length == 7
   puts "Game Over!"
   puts "The winning word was #{word_to_guess}"
 end
-
